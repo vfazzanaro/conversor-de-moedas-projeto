8e4703342de2668ce52c3aa01bcf251f669566ef
vfazzanaro <vfazzanaro@gmail.com>
2026-01-20 22:09:01 -0300

    Projeto Conversor de Moedas

---
+++ scripts.js
@@ -0,0 +1,44 @@
+
+const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
+
+function convertValues() {
+
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //valor em Real 
+    const currencyValueConverted = document.querySelector(".currency-value") //valor de outras moedas
+
+    const dolarToday = 5.38
+    const euroToday = 6.31
+    const bitcoinToday = 476.401
+
+    if (currencySelect.value == "dolar") { // se a moeda selecionada for dolar
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(inputCurrencyValue / dolarToday)
+    }
+    if (currencySelect.value == "euro") { // se o selecionado for euro
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(inputCurrencyValue / euroToday)
+    }
+    if (currencySelect.value == "bitcoin") { // se o selecionado for bitcoin
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC",
+        }).format(inputCurrencyValue / bitcoinToday)
+    }
+
+
+    currencyValueToConvert.innerHTML = inputCurrencyValue
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    }).format(inputCurrencyValue)
+
+    currencyValueConverted.innerHTML = convertedValue
+
+}
+convertButton.addEventListener("click", convertValues)
